c20ea21ec2d8ba5084cdf868038941f16ea1157d
yoon <46823971@naver.com>
2025-07-24 16:51:28 +0900

    .md modified commit

--- myshop_실습.sql
+++ myshop_실습.sql
@@ -303,3 +303,102 @@ format(ifnull(avg(total_due),0),0) 전체금액평균
 from order_header
 where left(order_date,7) between '2019-01' and '2019-06'
 group by left(order_date,4), substring(order_date,6,2) with rollup;
+
+/**
+	테이블 조인 : 기본 SQL 방식, ANSI SQL
+*/
+use myshop2019;
+select database();
+show tables;
+-- order_header - customer(customer_id), employee(employee_Id)
+
+-- Q01) 전체금액이 8,500,000 이상인 주문의 주문번호, 고객아이디, 사원번호, 주문일시, 전체금액을 조회하세요.
+select oh.order_id, c.customer_id, e.employee_id, oh.order_date, oh.total_due
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000;
+
+
+-- Q02) 위에서 작성한 쿼리문을 복사해 붙여 넣은 후 고객이름도 같이 조회되게 수정하세요.
+select oh.order_id, c.customer_id, c.customer_name, e.employee_id, oh.order_date, oh.total_due
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000;
+
+-- Q03) Q01 쿼리를 복사해 붙여 넣은 후 직원이름도 같이 조회되게 수정하세요.
+select oh.order_id, c.customer_id, e.employee_id, e.employee_name,oh.order_date, oh.total_due
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000;
+
+-- Q04) 위에서 작성한 쿼리문을 복사해 붙여 넣은 후 고객이름, 직원이름도 같이 조회되게 수정하세요.
+select oh.order_id, c.customer_id, c.customer_name, e.employee_id, e.employee_name,oh.order_date, oh.total_due
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000;
+
+
+-- Q05) 위에서 작성한 쿼리문을 복사해 붙여 넣은 후 전체금액이 8,500,000 이상인 '서울' 지역 고객만 조회되게 수정하세요.
+select oh.order_id, c.customer_id, c.customer_name, e.employee_id, e.employee_name,oh.order_date, oh.total_due , c.city
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000
+and city = '서울';
+
+-- Q06) 위에서 작성한 쿼리문을 복사해 붙여 넣은 후 전체금액이 8,500,000 이상인 '서울' 지역 '남자' 고객만 조회되게 수정하세요.
+select oh.order_id, c.customer_id, c.customer_name, e.employee_id, e.employee_name,oh.order_date, oh.total_due , c.city, c.gender
+from order_header oh, customer c, employee e
+where oh.customer_id = c.customer_id and oh.employee_id = e.employee_id
+and total_due >= 8500000
+and c.city = '서울'
+and c.gender = 'm';
+
+
+-- order_header -order_id- order_detail -product_id- product
+-- Q07) 주문수량이 30개 이상인 주문의 주문번호, 상품코드, 주문수량, 단가, 지불금액을 조회하세요.
+select oh.order_id, p.product_id, od.order_qty, od.unit_price, oh.total_due
+from order_header oh inner join order_detail od
+	 on oh.order_id = od.order_id
+	 inner join product p
+     on od.product_id = p.product_id
+where order_qty >= 30;
+
+
+-- Q08) 위에서 작성한 쿼리문을 복사해서 붙여 넣은 후 상품이름도 같이 조회되도록 수정하세요.
+select oh.order_id, p.product_id, p.product_name, od.order_qty, od.unit_price, oh.total_due
+from order_header oh inner join order_detail od
+	 on oh.order_id = od.order_id
+	 inner join product p
+     on od.product_id = p.product_id
+where order_qty >= 30;
+
+-- Q09) 상품코드, 상품이름, 소분류아이디를 조회하세요.
+-- product -sub_category_id- sub_category
+select p.product_id 상품코드, p.product_name 상품이름, sc.sub_category_id 소분류아이디
+from 
+product p inner join sub_category sc
+on p.sub_category_id = sc.sub_category_id;
+
+-- Q10) 위에서 작성한 쿼리문을 복사해서 붙여 넣은 후 소분류이름, 대분류아이디가 조회되게 수정하세요.
+select p.product_id 상품코드, p.product_name 상품이름, sc.sub_category_id 소분류아이디, sc.category_id 대분류아이디
+from 
+product p inner join sub_category sc
+on p.sub_category_id = sc.sub_category_id;
+
+-- Q11) 다정한 사원이 2019년에 주문한 상품명을 모두 출력해주세요.
+select e.employee_name, oh.order_date, p.product_name
+from employee e left outer join order_header oh
+on e.employee_id = oh.employee_id
+left outer join order_detail od
+on oh.order_id = od.order_id
+left outer join product p
+on od.product_id = p.product_id
+where e.employee_name = '다정한';
+
+-- Q12) 청소기를 구입한 고객아이디, 사원번호, 주문번호, 주문일시를 조회하세요.
+select c.customer_id, e.employee_id, p.product_id,p.product_name, oh.order_date
+from customer c, employee e, order_header oh, order_detail od, product p
+where c.customer_id = oh.customer_id and e.employee_id = oh.employee_id and oh.order_id = od.order_id and od.product_id = p.product_id
+and p.product_name like '%청소기%'; 
+
+
--- myspl_80_실습.sql
+++ myspl_80_실습.sql
@@ -671,6 +671,212 @@ from employee
 order by salary desc 
 limit 5;
 
+/******************************************
+	조인(Join) : 두 개 이상의 테이블을 연동해서 sql 실행
+    ERD(Endtity Relation Diagram) :데이터베이스 구조도(설계도)
+    - 데이터 모델링 : 정규화 과정
+    
+    ** ANSI SQL : 데이터베이스 시스템들의 표준 SQL
+    조인(Join) 종류
+    1) cross JOIN(CATEISIAN) - 합집합 : 테이블들의 데이터 전체를 조인 - 테이블A(10) * 테이블B(10)
+    2) INNER JOIN(NATURAL) - 교집합 : 두 개이상의 테이블을 조인 연결 고리를 통해 조인 실행
+    3) OUTER JOIN - INNER JOIN + 선택한 테이블의 조인 제외 ROW 포함
+    4) SELF JOIN : 한 테이블을 두 개 테이블처럼 조인해서 실행 
+******************************************/
+use hrdb2019;
+select database();
+select * from employee;
+select * from department;
+
+-- cross join
+select *
+from employee, department;
+
+select count(*)
+from employee cross join department;
+
+--
+select count(*) from vacation; -- 102
+-- 사원, 부서, 휴가 테이블 cross join : 20 * 7 * 102
+select count(*) from employee, department, vacation;
+select count(*) from employee cross join department cross join vacation;
+
+-- inner join
+select *
+from employee, department
+where employee.dept_id = department.dept_id
+order by emp_id;
+
+-- inner join : ansi
+select *
+from employee inner join department
+on employee.dept_id = department.dept_id
+order by emp_id;
+
+-- 사원테이블, 부서테이블, 본부테이블 inner join
+select *
+from employee e, department d, unit u
+where e.dept_id = d.dept_id
+and d.unit_id = u.unit_id
+order by e.emp_id;
+
+-- 사원테이블, 부서테이블, 본부테이블 inner join : ansi
+select *
+from employee e inner join department d 
+on e.dept_id = d.dept_id
+inner join unit u
+on d.unit_id = u.unit_id
+order by emp_id;
+
+-- 사원테이블, 부서테이블, 본부테이블, 휴가테이블 inner join : ansi
+select *
+from vacation v inner join employee e
+on v.emp_id = e.emp_id
+inner join department d
+on e.dept_id = d.dept_id
+inner join unit u
+on d.unit_id = u.unit_id;
+
+select *
+from  employee e, department d, unit u ,vacation v
+where e.dept_id = d.dept_id
+	and d.unit_id = u.unit_id
+    and e.emp_id = v.emp_id;
+
+-- 모든 사원들의 사번, 사원명, 부서아이디, 부서명, 입사일, 급여를 조회
+select e.emp_id, e.emp_name, e.dept_id, d.dept_name, e.hire_date, e.salary
+from employee e, department d
+where e.dept_id = d.dept_id
+order by emp_id;
+
+-- 영업부에 속한 사원들의 사번, 사원명, 입사일, 퇴사일, 폰번호, 급여, 부서아이디, 부서명 조회
+
+select e.emp_id, e.emp_name, e.hire_date, e.retire_date, e.phone, e.salary, e.dept_id, d.dept_name
+from employee e, department d
+where e.dept_id = d.dept_id
+	and d.dept_name = '영업';
+
+-- 인사과에 속한 사원들 중에 휴가를 사용한 사원들의 내역을 조회
+
+select *
+from employee e, vacation v, department d
+where e.emp_id = v.emp_id
+and d.dept_id = e.dept_id
+and dept_name = '인사';
+
+-- 영업부서 사원의 사원명, 폰번호, ,부서명, 휴가사용 이유 조회
+-- 휴가 사용 이유가 '두통'인 사원, 소속본부 조회
+select e.emp_name, e.phone, d.dept_name, v.reason, u.unit_name
+from employee e, department d, unit u, vacation v
+where e.dept_id = d.dept_id and u.unit_id = d.unit_id and e.emp_id = v.emp_id
+and d.dept_name = '영업'
+and reason = '두통';
+
+-- 2014년부터 2016년까지 입사한 사원들 중에서 퇴사하지 않은 사원들의
+-- 사원아이디, 사원명, 부서명, 입사일, 소속본부를 조회
+-- 소속본부 기준으로 오름차순 정렬
+
+select e.emp_id, e.emp_name, d.dept_name, e.hire_date, u.unit_name
+from employee e, department d, unit u
+where e.dept_id = d.dept_id and d.unit_id = u.unit_id
+and e.retire_date is null
+and left(ifnull(hire_date,0),4) between '2014' and '2016'
+order by u.unit_name;
+
+-- 부서별 총급여, 평균급여, 총휴가사용일수를 조회
+select d.dept_name 부서명,
+ concat(format(sum(e.salary),0),' 만원') 총급여, 
+ concat(format(avg(e.salary),2),' 만원') 평균급여, 
+ sum(duration) 횟수
+from employee e, department d, vacation v
+where e.dept_id = d.dept_id and e.emp_id = v.emp_id
+group by e.dept_id, d.dept_name;
+
+
+-- 본부별, 부서의  휴가사용 일수
+select u.unit_name, d.dept_name, d.dept_id, sum(duration) as 휴가사용일수
+from employee e, department d, vacation v, unit u
+where e.dept_id = d.dept_id
+	and e.emp_id = v.emp_id
+    and d.unit_id = u.unit_id
+group by d.dept_id, d.dept_name, u.unit_name;   
+
+-- outer join : inner join + 조인에서 제외된 row(테이블별 지정)
+-- 오라클 형식의 outer join은 사용불가, ansi sql 형식만 사용가능!!
+-- select [컬럼리스트] from [테이블명1 테이블별칭] left/right outer join 테이블명2 [테이블별칭],...]
+-- on [테이블명1.조인컬럼 = 테이블명2.조인컬럼]
+
+-- 오라클 형식 outer join (mysql에서는 사용불가!!)
+-- select * from table1 t1, table2 t2
+-- where t1.col = t2.col(+);
+
+-- 모든 부서의 부서아이디, 부서명, 본부명을 조회
+select d.dept_id, d.dept_name, u.unit_name
+from department d
+	 left outer join unit u 
+on d.unit_id = u.unit_id;
+
+-- 본부별, 부서의 휴가사용 일수 조회
+-- 부서의 누락없이 모두 출력
+select u.unit_name, d.dept_name, d.dept_id ,count(duration)
+from department d
+     left outer join employee e
+     on d.dept_id = e.dept_id
+     left outer join vacation v
+	 on e.emp_id = v.emp_id
+     left outer join unit u
+     on u.unit_id = d.unit_id
+group by u.unit_name, d.dept_id, d.dept_name
+order by unit_name;
+
+
+select  u.unit_name, d.dept_name, count(duration)
+from employee e left outer join vacation v
+	on e.emp_id = v.emp_id
+    right outer join department d
+    on e.dept_id = d.dept_id
+    left outer join unit u 
+    on d.unit_id = u.unit_id
+group by u.unit_name, d.dept_name
+order by u.unit_name;
+
+-- 2017년부터 2018년도까찌 입사한 사원들의 사원명, 입사일, 연봉, 부서명을 조회
+-- 단, 퇴사한 사원들 제외
+-- 소속본부를 모두 조회
+select e.emp_name , e.hire_date , ifnull(e.salary ,0), d.dept_name , ifnull(u.unit_name, '제-본부') as unit_name
+from employee e 
+	inner join department d
+on e.dept_id = d.dept_id
+	left outer join unit u
+on d.unit_id = u.unit_id
+where left(hire_date ,4) between '2017' and '2018' and retire_date is null; 
+ 
+-- self join : 자기 자신의 테이블을 조인
+-- self join은 서브쿼리 형태로 실행하는 경우가 많음!!
+-- select [컬럼리스트] from [테이블1], [테이블2] where [테이블1.컬럼명] = [테이블2.컬럼명]
+-- 사원테이블을 self join
+select e.emp_id , e.emp_name, m.emp_id, m.emp_name
+from employee e, employee m
+where e.emp_id = m.emp_id;
+
+-- 서브쿼리를 사용한 경우
+select emp_id , emp_name
+from employee 
+where emp_id = (select emp_id from employee where emp_name = '홍길동');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
