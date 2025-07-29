c9cff079097a7308e008cd2b22a765719a76de20
yoon <46823971@naver.com>
2025-07-29 16:18:34 +0900

    .md modified commit

--- myshop_실습.sql
+++ myshop_실습.sql
@@ -401,7 +401,34 @@ from customer c, employee e, order_header oh, order_detail od, product p
 where c.customer_id = oh.customer_id and e.employee_id = oh.employee_id and oh.order_id = od.order_id and od.product_id = p.product_id
 and p.product_name like '%청소기%';   
 
+/**
+	서브쿼리
+*/
+-- Q13) 'mtkim', 'odoh', 'soyoukim', 'winterkim' 고객 주문의 주문번호, 고객아이디, 주문일시, 전체금액을 조회하세요. 
+select oh.order_id , oh.customer_id, oh.order_date, oh.total_due
+	from order_header oh right outer join customer c
+    on oh.customer_id = c.customer_id
+    where c.customer_id in  ('mtkim' , 'odoh' ,'soyoukim','winterkim');
 
+-- Q14) '전주' 지역 고객의 아이디를 조회하세요.   
+select customer_id
+	from customer
+    where city = '전주';
+-- Q15) 위 두 쿼리문을 조합해서 하위 쿼리를 사용해 '전주' 지역 고객 주문의 주문번호, 고객아이디, 주문일시, 전체금액을 조회하세요.
+select order_id , customer_id, order_date, total_due
+	from order_header
+    where customer_id in (select customer_id from customer where city = '전주' and (customer_id in  ('mtkim' , 'odoh' ,'soyoukim','winterkim')));
+select * from customer
+where customer_id = 'odoh';
+
+-- Q16) 고객의 포인트 최댓값을 조회하세요.
+
+
+-- Q17) 하위 쿼리를 사용해 가장 포인트가 많은 고객의 이름, 아이디, 등록일, 포인트를 조회하세요.
+-- Q18) 하위 쿼리를 사용해 홍길동(gdhong) 고객보다 포인트가 많은 고객 이름, 아이디, 등록일, 포인트를 조회하세요.
+-- Q19) 하위 쿼리를 사용해 홍길동(gdhong) 고객과 같은 지역의 고객 이름, 아이디, 지역, 등록일, 포인트를 조회하세요.
+--      단, 고객 이름을 기준으로 오름차순 정렬해서 조회하세요.
+-- Q20) 하위 쿼리를 사용해 홍길동(gdhong) 고객보다 포인트가 많은 고객 이름, 아이디, 등록일, 포인트를 조회하고, 행번호를 추가하여 출력하세요.
 
 
 
